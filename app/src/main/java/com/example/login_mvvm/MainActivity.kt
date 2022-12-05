1094582457dfacccd71a6840c7c3f05ff67cd4e6
Walu064 <waluwalu064@gmail.com>
2022-12-05 10:44:30 +0100

    Empty project.

--- app/src/main/java/com/example/login_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/login_mvvm/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.login_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pum.wmex1.mvvm_1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
